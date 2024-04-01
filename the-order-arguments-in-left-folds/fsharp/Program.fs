2c61e42055f4f29581cb767b8ca140aa9ee8a1bc
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-04-01 23:48:48 +0200

    Add F# example for "The Order Of Arguments In Left Folds"

---
+++ the-order-arguments-in-left-folds/fsharp/Program.fs
@@ -0,0 +1,9 @@
+﻿let rev =
+    let collection = [1; 2; 3; 4; 5]
+    let initialAccumulator = []
+    let combiningFunction currentAccumulator currentItem =
+        currentItem :: currentAccumulator
+
+    List.fold combiningFunction initialAccumulator collection
+
+printfn "%A" rev
