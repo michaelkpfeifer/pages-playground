2f32add262810998dbc7450433e4e7f2d28d7e68
Michael Pfeifer <michael.k.pfeifer@googlemail.com>
2024-05-07 23:27:24 +0200

    Add Erlang example for "The Order Of Arguments In Left Folds"

---
+++ the-order-arguments-in-left-folds/erlang/fold.erl
@@ -0,0 +1,18 @@
+-module(fold).
+
+-export([run/0]).
+
+rev() ->
+    Collection = [1, 2, 3, 4, 5],
+    InitialAccumulator = [],
+    CombiningFunction =
+        fun(CurrentItem, CurrentAccumulator) -> [ CurrentItem | CurrentAccumulator ] end,
+
+    lists:foldl(
+      CombiningFunction,
+      InitialAccumulator,
+      Collection
+     ).
+
+run() ->
+    io:format("~p~n", [rev()]).
